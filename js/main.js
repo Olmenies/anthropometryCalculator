4bef942d9d94906bf3dc74005e477b59586e65f8
olmenies <olmedonies@gmail.com>
2022-02-13 23:55:44 -0300

    Fixed minor issues, removed commented code

--- js/main.js
+++ js/main.js
@@ -13,9 +13,11 @@
 6. Update dataInput as requried
 7. Create a for-loop to print the results of the different patients
 8. IT'D BE REALLY NEAT TO CREATE A TAB SYSTEM WHICH SWITCHES BETWEEN THE DATA INPUT AND THE RESULTS ---> FINAL PROYECT
-9. Add add functions to make fields required and verify the tpyes of the input
-10. Delete dataOutput() as it was useful when working by console || change the concatenation for a literal string
-11. Craneate how to save QUÉ CARAJO BORRÉ ACÁ?!?!?!
+9. Add functions to make fields required and verify the tpyes of the input
+10. Delete dataOutput() as it was useful when working by console || change the concatenation for a literal string *** DONE
+11. Craneate how to save QUÉ CARAJO BORRÉ ACÁ?!?!?! Add persons to the array?
+12. No se están agregando perosnas a las arrays, arreglar *** DONE
+13. Modificar el update del DOM para que represente al ARRAYPEOPLE
 */
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -43,8 +45,8 @@ function printLine()
 //function dataInput to verify the input type is correct *** This function should be deprecated as it was useful only when working with console ***
 function dataInput(_type)
 {
-  let _acc = 0, _input;
-  const _arrayValues = new Array(1);
+  let _acc = 0; //accumulator variable
+  let _input; //variable to temporary save the input introduced before checking if it's correct
 
   switch (_type)
   {
@@ -82,34 +84,16 @@ function dataInput(_type)
   }//end of switch
 }//end of verifyInput function
 
-//function dataOutput to print the results
-function dataOutput(_person)
-{
-  printLine();
-  console.log("Nombre: " + _person.name + "\nApellido: " + _person.surname + "\nNombre completo: " + _person.fullname + "\nEdad:" + _person.age + "\nAltura: " + _person.height + "\nPeso: " + _person.weight + "\nIMC: " + _person.IMC ); //change to literal template
-  printLine();
-}//end of function
-
 //function saveData to create a new person and print the results on the DOM. It has to be divided in three different functions
 function saveData()
 {
-  const arrayPeople = []; // Array to save people
   const person = new Person(); //instanciation of a new person
-  const flag = arrayPeople.find((el) => el.fullname === person.fullname); //we evaluate if a person with the same fullname already exists
-  /*
-  console.log(person.name);
-  console.log(person.surname);
-  console.log(person.fullname);
-  console.log(person.age);
-  console.log(person.height);
-  console.log(person.weight);
-  console.log(person.IMC);
-  */
+  const flag = arrayPeople.some((el) => { return el.fullname === person.fullname}); //we evaluate if a person with the same fullname already exists --> Flag is alwas undefined
 
   //if a person with the same fullname already exists...
-  if(flag)
+  if(flag == true)
   {
-
+    console.log("I'm on the if");
     //we'll iterate over the array until we find the element with the sane fullname
     for(const element of arrayPeople){
 
@@ -126,22 +110,31 @@ function saveData()
   //else, we add a new person to the array ---------------------> ***is this fine or should I concatenate a new array?***
   else
   {
+    console.log("I'm on the else");
     arrayPeople.push(person);
   }//end of else
 
-  //to do: the for-loop to add divs on the body ***Replace this with a ul-li***
-  let results = document.createElement("div");
-  results.innerHTML = `
-  <h2 class="mt-5"> Resultados: </h2>
-  <span>Nombre: ${person.name}</span>
-  <span>Apellido: ${person.surname}</span>
-  <span>Código: ${person.fullname}</span>
-  <span>Edad: ${person.age}</span>
-  <span>Altura: ${person.height}</span>
-  <span>Peso: ${person.weight}</span>
-  <span>IMC: ${person.IMC}</span>
-  `;
-  document.getElementById("fatherResults").appendChild(results);
+
+  for(let i=0; i<= arrayPeople.length; i++){
+    console.log(arrayPeople[i]);
+  }//end of for loop
+
+  //to do: the for-loop to add divs on the body ***Replace this with a ul-li*** MAKE THIS TO PRINT THE ARRAYPEOPLE
+  for(const el of arrayPeople)
+  {
+    let results = document.createElement("div");
+    results.innerHTML = `
+    <h2 class="mt-5"> Resultados: </h2>
+    <span>Nombre: ${person.name}</span>
+    <span>Apellido: ${person.surname}</span>
+    <span>Código: ${person.fullname}</span>
+    <span>Edad: ${person.age}</span>
+    <span>Altura: ${person.height}</span>
+    <span>Peso: ${person.weight}</span>
+    <span>IMC: ${person.IMC}</span>
+    `;
+    document.getElementById("fatherResults").appendChild(results);
+  }//end of for-of loop
 }//end of saveData function
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -169,48 +162,6 @@ class Person
 
 console.log("Bienvenido a la calculadora de antropometrías");
 
-//let option; //Option for the user to choose
-//option = prompt("Ingresar 1 para ejecutar o cualquier otra tecla para salir");
-
+const arrayPeople = []; // Array to save people. Made global to make it usable on the different functions
 let element = document.getElementById("buttonCalculate");
 element.addEventListener("click", saveData);
-
-/*
-//we'll iterate until a option different of 1 is instroduced
-do{
-if(option == 1)
-{
-const person = new Person(); //we instanciate a new person
-const flag = arrayPeople.find((el) => el.fullname === person.fullname); //we evaluate if a person with the same fullname already exists
-
-//if a person with the same fullname already exists...
-if(flag){
-
-//we'll iterate over the array until we find the element with the sane fullname
-for(const element of arrayPeople){
-
-//we change the values for the element
-if(element.fullname === person.fullname){
-element.age = person.age;
-element.height = person.height;
-element.weight = person.weight;
-element.IMC = person.IMC;
-}//end of internal if
-}//end of for
-}//end of external if
-
-else {
-console.log("estoy en el else");
-arrayPeople.push(person);
-} //end of else
-
-dataOutput(person); //we print the results on the console
-
-option = prompt("Ingresar 1 para ejecutar el programa o cualquier otra tecla para salir");
-}//end of if
-
-else{
-break;
-}//end of else
-} while(option == 1);
-*/
